d78cfe2cfaf426135147d29f001121ff0e2df881
mrzkyalamysh <rizky002.mra@gmail.com>
2022-06-22 12:56:14 +0700

    uasdpm

---
+++ app/src/main/java/com/example/uas_dpm_muhammad_rizki_alamsyah/DataDosen.kt
@@ -0,0 +1,11 @@
+package com.example.uas_dpm_muhammad_rizki_alamsyah
+
+class DataDosen(
+    var Nidn: String,
+    var NmDosen: String,
+    var jabatan: String,
+    var GolPangkat: String,
+    var pendidikan: String,
+    var keahlian: String,
+    var ProgramStudi:String
+)
